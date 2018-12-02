efa7ac5b05b81542e501ca82871a721645c6706d
Baxlan <cavalioz@hotmail.fr>
2018-12-02 17:34:59 +0100

    BSD licence has been updated (setting the author's name and the date on 2016)

--- include/omniunit/units/luminous_intensity.hh
+++ include/omniunit/units/luminous_intensity.hh
@@ -1,16 +1,18 @@
 //luminous_intensity.hh
 
 /*
-Copyright (c) 1998, Regents of the University of California All rights
-reserved. Redistribution and use in source and binary forms, with or without
+Copyright (c) 2016, Denis Tosetto All rights reserved.
+Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
   Redistributions of source code must retain the above copyright notice,
   this list of conditions and the following disclaimer.
+
   Redistributions in binary form must reproduce the above copyright notice,
   this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
-  Neither the name of the University of California, Berkeley nor the names
+
+  Neither the name of Denis Tosetton, nor Baxlan nor the names
   of its contributors may be used to endorse or promote products derived
   from this software without specific prior written permission.
 
